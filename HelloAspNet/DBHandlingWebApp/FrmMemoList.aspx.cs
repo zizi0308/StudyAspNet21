46c4dec504306d462c1dfd8e1cad603aa0a77d31
JO HUI JI <whgmlwl222@naver.com>
2021-04-13 17:01:47 +0900

    210413 화요일 오후수업 커밋

---
+++ HelloAspNet/DBHandlingWebApp/FrmMemoList.aspx.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DBHandlingWebApp
+{
+    public partial class FrmMemoList : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            using (var conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+
+                SqlCommand cmd = new SqlCommand("ListMemo", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Memos");
+
+                GrdMemoList.DataSource = ds;
+                GrdMemoList.DataBind();
+            }
+        }
+
+        protected void BtnSearch_Click(object sender, EventArgs e)
+        {
+            var connString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+            using (var conn = new SqlConnection(connString))
+            {
+                if (conn.State == ConnectionState.Closed) conn.Open();
+
+                SqlCommand cmd = new SqlCommand("SearchMemo", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@SearchField", CboSearch.SelectedValue); // Name, Title
+                cmd.Parameters.AddWithValue("@SearchQuery", TxtSearch.Text.Replace("--", ""));
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataSet ds = new DataSet();
+                adapter.Fill(ds, "Memos");
+
+                GrdMemoList.DataSource = ds.Tables[0].DefaultView; // ds만 써도 무방
+                GrdMemoList.DataBind();
+            }
+        }
+    }
+}
---
+++ HelloAspNet/DBHandlingWebApp/FrmMemoView.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DBHandlingWebApp
+{
+    public partial class FrmMemoView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(Request.Params["Num"]))
+            {
+                Response.Write("잘못된 요청입니다.");
+                Response.End();
+            }
+            else
+            {
+                var connString = ConfigurationManager.ConnectionStrings["ConnString"].ConnectionString;
+                using (var conn = new SqlConnection(connString))
+                {
+                    if (conn.State == ConnectionState.Closed) conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("ViewMemo", conn);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Num", Convert.ToInt32(Request["Num"]));
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        LblNum.Text = Request["Num"];
+                        LblName.Text = reader["Name"].ToString();
+                        LblEmail.Text = reader["Email"].ToString();
+                        LblTitle.Text = reader["Title"].ToString();
+                        LblPostDate.Text = reader["PostDate"].ToString();
+                        LblPostIP.Text = reader["PostIP"].ToString();
+                    }
+
+                    reader.Close();
+                }
+            }
+        }
+    }
+}
