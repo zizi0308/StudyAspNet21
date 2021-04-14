e12f9ff280b38229cbd9f0e231f2d7347f911f69
JO HUI JI <whgmlwl222@naver.com>
2021-04-14 15:17:34 +0900

    로그인페이지 커밋

---
+++ HelloAspNet/LoginWebApp/Login.aspx.cs
@@ -0,0 +1,41 @@
+﻿using LoginWebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginWebApp
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtnLogin_Click(object sender, EventArgs e)
+        {
+            var repo = new Repository();
+            if (repo.IsCorrectUser(TxtUserID.Text, TxtPassword.Text))
+            {
+                if (string.IsNullOrEmpty(Request["ReturnUrl"]))
+                {
+                    FormsAuthentication.RedirectFromLoginPage(TxtUserID.Text, false);
+                }
+                else
+                {
+                    FormsAuthentication.SetAuthCookie(TxtUserID.Text, false); // 인증된 사용자아이디를 추가
+                    Response.Redirect("~/Welcome.aspx");
+                }
+            }
+            else
+            {
+                Response.Write("<script>alert('잘못된 사용자입니다');</script>");
+                Response.End();
+            }
+        }
+    }
+}
---
+++ HelloAspNet/LoginWebApp/Welcome.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Welcome.aspx.cs" Inherits="LoginWebApp.Welcome" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>회원관리-로그인페이지</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>로그인확인</h2>
+            <asp:Label ID="LblName" runat="server"></asp:Label>님, 반갑습니다!
+        </div>
+    </form>
+</body>
+</html>
