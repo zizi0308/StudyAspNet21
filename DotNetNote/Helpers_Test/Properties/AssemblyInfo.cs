88225b3a570731a906f242f2666cda55bf371322
JO HUI JI <whgmlwl222@naver.com>
2021-04-14 17:24:13 +0900

    오후수업 커밋

---
+++ DotNetNote/Helpers_Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Helpers_Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Helpers_Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2901af01-136c-4369-9792-1cf1b69858af")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DotNetNote/Helpers_Test/StringLibrayTest.cs
@@ -0,0 +1,41 @@
+﻿using Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Helpers_Test
+{
+    [TestClass]
+    public class StringLibrayTest
+    {
+        [TestMethod]
+        public void CutStringTest()
+        {
+            string strCut = "Hello World, This is a sentence";
+            int intChar = 15;
+
+            var expected = "Hello World,...";
+            var actual = StringLibray.CutString(strCut, intChar);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void CutStringUnicodeTest()
+        {
+            string strCut = "안녕하세요. 부경대학교입니다.";
+            int intChar = 9;
+
+            var expected = "안녕하세요....";
+            var actual = StringLibray.CutString(strCut, intChar);
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            var expected = 10;
+            var actual = (5 + 5);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
